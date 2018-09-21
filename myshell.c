b6b0693ed893c9934bdfbc7d761e7b3daa813b23
tuh37195 <tuh37195@temple.edu>
2018-09-21 10:57:52 -0400

    Added some basic code outline

---
+++ myshell.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define FALSE 0
+#define TRUE 1
+
+//get input from stdin, return it as a string
+char *get_input(){
+
+}
+
+//breaks the input up into individual commands
+char **parse_input(){
+
+}
+//processes the input and execute the desired commands
+void process_input(){
+
+}
+
+//returns current directory
+char *get_dir(){
+
+}
+
+//print current directory
+void print_dir(){
+
+}
+
+//change the current directory
+void change_dir(){
+
+}
+
+//clears the terminal
+void clear_terminal(){
+
+}
+
+//returns the input as a string
+void echo(){
+
+}
+
+//pauses the terminal until the enter key is pressed
+void pause(){
+
+}
+
+//displays a list of commands
+void help(){
+
+}
+
+//exit the program
+void escape(){
+  exit(0);
+}
+
+int main(){
+
+  while(TRUE){
+    //get input
+    //parse input
+    //process commands
+  }
+}
